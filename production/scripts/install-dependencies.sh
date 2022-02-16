f58be0e8def3568fa1b79cbac3b015f188ae956f
Rafhael Payão <rafaeltmbr@gmail.com>
2022-02-16 00:26:03 -0300

    ci(github): improved the workflow

--- production/scripts/install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# install docker if necessary
-which docker || sudo ./install-docker.sh
-
-# install docker-compose if necessary
-which docker-compose || sudo ./install-docker-compose.sh
