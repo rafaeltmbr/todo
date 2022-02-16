17423125b071726b60aa1c9adcebaa04c1f5c835
Rafhael Payão <rafaeltmbr@gmail.com>
2022-02-15 23:44:02 -0300

    ci(github): improved the workflow

--- production/scripts/install-docker.sh
+++ production/scripts/install-docker.sh
@@ -6,15 +6,15 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # 1 - update the apt package index
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 # 2 - add docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # 3 - setup a stable repository
 echo \
@@ -22,8 +22,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # 4 - install docker
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # 5 - create the docker group
 sudo groupadd docker
