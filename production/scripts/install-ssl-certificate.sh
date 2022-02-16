ebee281cca9dc308f2f5275fd52334dfe839e506
Rafhael Payão <rafaeltmbr@gmail.com>
2022-02-15 21:57:14 -0300

    ci(github): attempt to improve the workflow

--- production/scripts/install-ssl-certificate.sh
+++ production/scripts/install-ssl-certificate.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
-if [ ! -f nginx/templates/https.conf.template ]; then
-  docker run --rm --volume "$(pwd)/certbot/www:/var/www/html:rw" --volume "$(pwd)/certbot/certificate:/etc/letsencrypt:rw" \
-  certbot/certbot:latest certonly --webroot -w /var/www/html -d $1 --agree-tos --email $2 -n && \
-  mv nginx/templates/https.conf.template_disabled nginx/templates/https.conf.template && \
+if [ ! -f nginx/conf.d/https.conf ]; then
+  docker run --rm \
+    --volume "$(pwd)/certbot/www:/var/www/html:rw" \
+    --volume "$(pwd)/certbot/certificate:/etc/letsencrypt:rw" \
+    certbot/certbot:latest certonly --webroot -w /var/www/html \
+    -d $1 --agree-tos --email $2 -n && \
+  mv nginx/conf.d/https.conf.disabled nginx/conf.d/https.conf && \
   docker-compose restart nginx  
 fi
